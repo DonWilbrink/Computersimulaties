f081ee62811fe8de5928b780034ce04bb2323b57
Don Wilbrink <don.wilbrink@gmail.com>
2025-05-04 00:28:12 +0200

    Programma pythkleed toegevoegd

--- main.pas
+++ main.pas
@@ -5,18 +5,28 @@ unit main;
 interface
 
 uses
-  Classes, SysUtils, Forms, Controls, Graphics, Dialogs;
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, Menus, ExtCtrls;
 
 type
-  TForm1 = class(TForm)
-  private
 
+  { TfrmMain }
+
+  TfrmMain = class(TForm)
+    Hoofdminu: TMainMenu;
+    mniHoofdstuk2: TMenuItem;
+    mniPythkleed: TMenuItem;
+    mniHoofdstuk1: TMenuItem;
+    mniHoofdstukken: TMenuItem;
+    pbMain: TPaintBox;
+    procedure mniPythkleedClick(Sender: TObject);
+  private
+    procedure pbClear;
   public
 
   end;
 
 var
-  Form1: TForm1;
+  frmMain: TfrmMain;
   prog, xOff, yOff: Integer;
   xFac, yFac: Double;
   EgaColor : array[0..15] of TColor =
@@ -30,6 +40,56 @@ var
 implementation
 
 {$R *.lfm}
+
+{ TfrmMain }
+
+procedure TfrmMain.mniPythkleedClick(Sender: TObject);
+var
+  m, max, n, x, xm, y, ym: Integer;
+begin
+  prog := 1;
+  pbClear;
+  frmMain.Caption := 'Computer simulaties: pythagoraskleed';
+  xm := pbMain.Width div 2;
+  ym := pbMain.Height div 2;
+  max := 300;
+  with pbMain.Canvas do
+  begin
+    Line(xm-max,ym+max,xm+max,ym-max);
+    for m := 0 to max do
+    begin
+      n := 0;
+      while n <= m do
+      begin
+        x := m*m-n*n;
+        y := 2*m*n;
+        if (x < max) and (y < max) then
+        begin
+          Pixels[xm+x,ym-y] := clYellow;
+          Pixels[xm+x,ym+y] := clYellow;
+          Pixels[xm-x,ym-y] := clYellow;
+          Pixels[xm-x,ym+y] := clYellow;
+          Pixels[xm+y,ym-x] := clYellow;
+          Pixels[xm+y,ym+x] := clYellow;
+          Pixels[xm-y,ym-x] := clYellow;
+          Pixels[xm-y,ym+x] := clYellow;
+        end;
+        Inc(n);
+      end;
+    end;
+  end;
+end;
+
+procedure TfrmMain.pbClear;
+begin
+  with pbMain.Canvas do
+  begin
+    Clear;
+    Brush.Color := clBlack;
+    Pen.Color := clYellow;
+  end;
+
+end;
 
 end.
 
