b3f5827b89ddac44acdddcec3e410109afc3b6e9
Don Wilbrink <don.wilbrink@gmail.com>
2025-05-10 23:54:13 +0200

    Programma's priem en gauss toegevoegd

--- main.pas
+++ main.pas
@@ -5,19 +5,29 @@ unit main;
 interface
 
 uses
-  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, Menus, ExtCtrls;
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, Menus, ExtCtrls,
+  StdCtrls, Windows;
 
 type
 
   { TfrmMain }
 
   TfrmMain = class(TForm)
+    edtInvoer: TEdit;
     Hoofdminu: TMainMenu;
+    lblInvoer: TLabel;
+    memUitvoer: TMemo;
+    mniGauss: TMenuItem;
+    mniPriem: TMenuItem;
     mniHoofdstuk2: TMenuItem;
     mniPythkleed: TMenuItem;
     mniHoofdstuk1: TMenuItem;
     mniHoofdstukken: TMenuItem;
+    Panel1: TPanel;
     pbMain: TPaintBox;
+    procedure FormCreate(Sender: TObject);
+    procedure mniGaussClick(Sender: TObject);
+    procedure mniPriemClick(Sender: TObject);
     procedure mniPythkleedClick(Sender: TObject);
   private
     procedure pbClear;
@@ -55,7 +65,7 @@ begin
   max := 300;
   with pbMain.Canvas do
   begin
-    Line(xm-max,ym+max,xm+max,ym-max);
+    Rectangle(xm-max,ym+max,xm+max,ym-max);
     for m := 0 to max do
     begin
       n := 0;
@@ -80,6 +90,109 @@ begin
   end;
 end;
 
+procedure TfrmMain.mniPriemClick(Sender: TObject);
+var
+  T1, T2, FProc: Int64;
+  f, j, n : Integer;
+begin
+  prog := 2;
+  pbClear;
+  frmMain.Caption := 'Computer simulaties: Factorisatie getal < 2147483648';
+  Panel1.Visible := True;
+  QueryPerformanceFrequency(Fproc);     // Get processor frequency
+  lblInvoer.Caption := 'Geef getal < 2147483648';
+  n := StrToInt(edtInvoer.Text);
+  j := 1;
+  memUitvoer.Clear;
+  QueryPerformanceCounter(T1);          // Start
+  While n mod 2 = 0 do
+  begin
+    memUitvoer.Lines.Add(j.ToString + '  2');
+    Inc(j);
+    n := n div 2;
+  end;
+  f := 3;
+  while n mod 3 = 0 do
+  begin
+    memUitvoer.Lines.Add(j.ToString + '  3');
+    Inc(j);
+    n := n div 3;
+  end;
+  while f < Sqrt(n) do
+  begin
+    Inc(f,2);
+    while n mod f = 0 do
+    begin
+      memUitvoer.Lines.Add(j.ToString + '  ' + f.ToString);
+      Inc(j);
+      n := n div f;
+    end;
+  end;
+  QueryPerformanceCounter(T2);
+  if n > 1 then
+  begin
+    memUitvoer.Lines.Add(j.ToString + '  ' + n.ToString);
+    memUitvoer.Lines.Add('');
+    memUitvoer.Lines.Add('Het factoriseren duurde: ' + (1000*(T2-T1)/FProc).ToString + ' milliseconden');
+  end;
+end;
+
+procedure TfrmMain.mniGaussClick(Sender: TObject);
+var
+  n, n1, n2: Integer;
+  a, b, f, f1, f2, s: Double;
+  label nextpoint;
+begin
+  prog := 3;
+  pbClear;
+  frmMain.Caption := 'Computer simulaties: Priemgetallenkleedje van Gauss';
+  xOff := pbMain.Width div 2;
+  yOff := pbMain.Height div 2;
+  xFac := pbMain.Width/80;
+  yFac := pbMain.Height/80;
+  n := 40;
+  with pbMain.Canvas do
+  begin
+    Rectangle(Round(xOff+xFac*(-n-2)),Round(yOff+yFac*(-n-2)),Round(xOff+xFac*(n+2)),Round(yOff+yFac*(n+2)));
+    for n2 := 0 to n do
+    begin
+      for n1 := n2 to n do
+      begin
+        s := Sqrt(n1*n1+n2*n2);
+        f2 := 0;
+        repeat
+          f1 := 0;
+          repeat
+            f := f1*f1+f2*f2;
+            if (f <> 0) and (f <> 1) then
+            begin
+              a := (n1*f1+n2*f2)/f;
+              b := Abs(n1*f2-n2*f1)/f;
+              if (a - Trunc(a) = 0) and (b - Trunc(b) = 0) then Goto nextpoint;
+            end;
+            f1 := f1 + 1;
+          until (f1 * f1 + f2 * f2 > s);
+          f2 := f2 + 1;
+        until (f2 * f2 > s);
+        EllipseC(Round(xOff+xFac*n1),Round(yOff+yFac*n2),3,3);
+        EllipseC(Round(xOff+xFac*n2),Round(yOff+yFac*n1),3,3);
+        EllipseC(Round(xOff+xFac*-n1),Round(yOff+yFac*n2),3,3);
+        EllipseC(Round(xOff+xFac*-n2),Round(yOff+yFac*n1),3,3);
+        EllipseC(Round(xOff+xFac*-n1),Round(yOff+yFac*-n2),3,3);
+        EllipseC(Round(xOff+xFac*-n2),Round(yOff+yFac*-n1),3,3);
+        EllipseC(Round(xOff+xFac*n1),Round(yOff+yFac*-n2),3,3);
+        EllipseC(Round(xOff+xFac*n2),Round(yOff+yFac*-n1),3,3);
+        nextpoint:
+      end;
+    end;
+  end;
+end;
+
+procedure TfrmMain.FormCreate(Sender: TObject);
+begin
+  pbClear;
+end;
+
 procedure TfrmMain.pbClear;
 begin
   with pbMain.Canvas do
@@ -87,6 +200,7 @@ begin
     Clear;
     Brush.Color := clBlack;
     Pen.Color := clYellow;
+    Panel1.Visible := False;
   end;
 
 end;
